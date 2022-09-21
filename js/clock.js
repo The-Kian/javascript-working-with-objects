10f8fc61af2b6d2f17dce6812fe929fd567c2218
The-Kian <kian.popat@gmail.com>
2022-09-21 11:09:49 +0100

    comments

--- js/clock.js
+++ js/clock.js
@@ -2,36 +2,36 @@
     console.info("Hello JS"); 
 })();
 
-var myNode = document.createElement('div');
-document.getElementById('digitalClock').appendChild(myNode);
+var myNode = document.createElement('div'); //create divider
+document.getElementById('digitalClock').appendChild(myNode); //create child in Digital clock div
 myNode.innerHTML = "Welcome";
 
 
 
-var checkForLeadingZeros = function (timeVal){
-    if (timeVal<10){
-        timeVal = "0"+timeVal;
+var checkForLeadingZeros = function (timeVal){ //variable to check for and add leading zeros
+    if (timeVal<10){    //if value is less than 2 digits
+        timeVal = "0"+timeVal; //add a 0 string
     }
-    return timeVal;
+    return timeVal;//return string
 }
 
-var updateTime = function(){
-    var myDate = new Date();
-    var myHours = checkForLeadingZeros(myDate.getHours());
-    var myMins = checkForLeadingZeros(myDate.getMinutes());
-    var mySeconds = checkForLeadingZeros(myDate.getSeconds());
-    var myTimeDisplay = (myHours + ":"+ myMins +":"+ mySeconds);
-    myNode.innerHTML = myTimeDisplay;
+var updateTime = function(){//function to update time
+    var myDate = new Date();//store date in mydate
+    var myHours = checkForLeadingZeros(myDate.getHours());//take hours from myDate and check for zeros
+    var myMins = checkForLeadingZeros(myDate.getMinutes());//take mins from myDate and check for zeros
+    var mySeconds = checkForLeadingZeros(myDate.getSeconds());//take secs
+    var myTimeDisplay = (myHours + ":"+ myMins +":"+ mySeconds);//display string hrs:mins:secs
+    myNode.innerHTML = myTimeDisplay;//display myTimeDisplay in myNode div
     console.info('tick tock');
-    var rotSeconds = (mySeconds*6)-90;
-    var rotMins = (myMins*6)-90;
-    var rotHours = (myHours %12 + myMins/60)*30 -90;
-    document.getElementById('minHand').style.transform = 'rotate('+rotMins+'deg)';
+    var rotSeconds = (mySeconds*6)-90;//angle for second hand
+    var rotMins = (myMins*6)-90;//angle for minute hand
+    var rotHours = (myHours %12 + myMins/60)*30 -90; //angle for hours hand
+    document.getElementById('minHand').style.transform = 'rotate('+rotMins+'deg)';//rotate by allocated angle
     document.getElementById('secondHand').style.transform = 'rotate('+rotSeconds+'deg)';
     document.getElementById('hrHand').style.transform = 'rotate('+rotHours+'deg)';
 }
 
 
-setInterval(updateTime,1000);
+setInterval(updateTime,1000);//run updateTime every second
 
 document.getElementById('hrHand').style.backgroundColor = '#0f0';
--- js/staff.js
+++ js/staff.js
@@ -15,36 +15,39 @@
 //     email : "j.grant@busy.com" }
 // ]
 
-var staff = JSON.parse(localStorage.getItem('staffList'));
+var staff = JSON.parse(localStorage.getItem('staffList')); //get staff list from local storage
 console.log(staff);
-if (staff === null){
-    staff=[];
+if (staff === null){ //if nothing in localStorage
+    staff=[]; //staff is empty
 }
 
 
-document.getElementById('addStaffForm').addEventListener('submit',function(ev){
-    ev.preventDefault();
+document.getElementById('addStaffForm').addEventListener('submit',function(ev){ 
+    //if submit event occurs in addStaffForm run function
+    ev.preventDefault();//NOT SURE
+    //add form value to variables
     let newStaffName = document.getElementById('staffName').value;
     let newStaffEmail = document.getElementById('staffEmail').value;
-    staff.push({name:newStaffName, email:newStaffEmail});
-    console.dir(staff);
+    staff.push({name:newStaffName, email:newStaffEmail}); //add vars to staff
+    console.dir(staff); 
     rebuildList();
 })
 
 
-var rebuildList=function(){
-    staff.forEach(function (element) {
-        var newStaffRow = document.createElement("tr");
-        var newStaffName = document.createElement("td");
-        newStaffName.innerHTML = element.name;
-        var newStaffEmail = document.createElement("td");
-        newStaffEmail.innerHTML=element.email;
-        newStaffRow.appendChild(newStaffName);
+var rebuildList=function(){ 
+    document.getElementById('staffTable').innerHTML = "";//not sure
+    staff.forEach(function (element) {//run through every element of staff
+        var newStaffRow = document.createElement("tr"); //create new row
+        var newStaffName = document.createElement("td"); //create table square
+        newStaffName.innerHTML = element.name;//add name to table
+        var newStaffEmail = document.createElement("td"); //create table square
+        newStaffEmail.innerHTML=element.email;//add email to table
+        newStaffRow.appendChild(newStaffName); //adds a node
         newStaffRow.appendChild(newStaffEmail);
         document.getElementById('staffTable').appendChild(newStaffRow);
         });
-        localStorage.setItem('staffList', JSON.stringify(staff));
-        //document.getElementById('staffTable').innerHTML = "";
+        localStorage.setItem('staffList', JSON.stringify(staff));//save staff to local storage
+        
 }
 
 rebuildList();
