cec780663ab23a65fc0dddd7634413d73e317925
The-Kian <kian.popat@gmail.com>
2022-09-21 10:11:51 +0100

    Staff List Almost done (tabled not cleared)

--- js/staff.js
+++ js/staff.js
@@ -3,25 +3,48 @@
 })();
 
 
-var staff = [
-    {
-    name : "Bob Smith",
-    email : "b.smith@busy.com"
-    }, {
-    email : "j.johnson@busy.com" },
-    {
-    name : "Ethan Hawkson", email : "e.hawkson@busy.com"
-    }, {
-    email : "j.grant@busy.com" }
-]
+// var staff = [
+//     {
+//     name : "Bob Smith",
+//     email : "b.smith@busy.com"
+//     }, {
+//     email : "j.johnson@busy.com" },
+//     {
+//     name : "Ethan Hawkson", email : "e.hawkson@busy.com"
+//     }, {
+//     email : "j.grant@busy.com" }
+// ]
 
-staff.forEach(function (element) {
-    var newStaffRow = document.createElement("tr");
-    var newStaffName = document.createElement("td");
-    newStaffName.innerHTML = element.name;
-    var newStaffEmail = document.createElement("td");
-    newStaffEmail.innerHTML=element.email;
-    newStaffRow.appendChild(newStaffName);
-    newStaffRow.appendChild(newStaffEmail);
-    document.getElementById('staffTable').appendChild(newStaffRow);
-});
+var staff = JSON.parse(localStorage.getItem('staffList'));
+console.log(staff);
+if (staff === null){
+    staff=[];
+}
+
+
+document.getElementById('addStaffForm').addEventListener('submit',function(ev){
+    ev.preventDefault();
+    let newStaffName = document.getElementById('staffName').value;
+    let newStaffEmail = document.getElementById('staffEmail').value;
+    staff.push({name:newStaffName, email:newStaffEmail});
+    console.dir(staff);
+    rebuildList();
+})
+
+
+var rebuildList=function(){
+    staff.forEach(function (element) {
+        var newStaffRow = document.createElement("tr");
+        var newStaffName = document.createElement("td");
+        newStaffName.innerHTML = element.name;
+        var newStaffEmail = document.createElement("td");
+        newStaffEmail.innerHTML=element.email;
+        newStaffRow.appendChild(newStaffName);
+        newStaffRow.appendChild(newStaffEmail);
+        document.getElementById('staffTable').appendChild(newStaffRow);
+        });
+        localStorage.setItem('staffList', JSON.stringify(staff));
+        //document.getElementById('staffTable').innerHTML = "";
+}
+
+rebuildList();
